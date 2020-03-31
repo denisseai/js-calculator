acb834677f26ebb59fc064f360d99c8264c6875e
denisseai <msdenisseanaya@gmail.com>
2020-03-31 15:24:33 -0700

    comments on what to do

---
+++ calculator.js
@@ -0,0 +1,12 @@
+// Program should ask user for first number, second number and operation
+
+// User input because no cli was made
+
+
+// valid options for operation
+
+
+// Provide a result of applying the opreation to the two numbers given (personal note: using if-statements)
+// Handle null input and dispay message if true
+
+// Display result
